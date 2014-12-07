f07853d76c9d48c333e3e3f9f2abfd0e88e65622
Juan Ruiz <j.ruiz3750@gmail.com>
2014-12-07 21:30:40 +0000

    Signed off by Juan Ruiz

--- CSC11/prob3.s
+++ CSC11/prob3.s
@@ -3,13 +3,13 @@ inFrmt: .asciz "%d"
 
 inNum: .word 0
 
-rtMsg: .asciz "Enter number whose root you want: "
+rtMsg: .asciz "Enter number(1-10,000) whose root you want: "
 
 .balign 4
 numRead: .asciz "Num read is %d\n"
 
 .balign 4
-floatRead: .asciz "Float is %f\n"
+rootIs: .asciz "Square root is %f\n"
 
 .balign 4
 half: .float 0.5
@@ -59,18 +59,16 @@ main:
     ldr r0, =xNot                /* save the value for next iteration */
     vstr s12, [r0]
 
+    sub r5, r5, #1
+    cmp r5, #0
+    bne bLoop
+      
     /* for printing */
     vcvt.f64.f32 d0, s12
 
     vmov r2, r3, d0
-    ldr r0, =floatRead
+    ldr r0, =rootIs
     bl printf
-
-    sub r5, r5, #1
-    cmp r5, #0
-    bne bLoop
-
-      
 
     pop {r4, r5, r6,  lr}
     bx lr
