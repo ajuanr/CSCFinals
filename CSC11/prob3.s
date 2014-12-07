4bc2478561d06b22d0b7f510f7d7dbf79ac6fab7
Juan Ruiz <j.ruiz3750@gmail.com>
2014-12-07 21:24:12 +0000

    Signed off by Juan Ruiz

--- CSC11/prob3.s
+++ CSC11/prob3.s
@@ -37,26 +37,39 @@ main:
 
     ldr r1, adr_inNum
     ldr r1, [r1]
-    vmov s10, r1                 /* s10 holds the num */
+    vmov s10, r1                 /* s10 holds the num S*/
 
     vcvt.f32.s32 s10, s10        /* convert number to float */
 
     ldr r0, =half
     vldr s11, [r0]               /* s11 holds 1/2 */
+
+
+    mov r5, #10                   /* r5 holds counter */
+    bLoop:
     ldr r0, =xNot
     vldr s12, [r0]               /* s12 holds initial guess xNot */
 
-    vdiv.f32 s10, s10, s12 
-    vadd.f32 s10, s10, s12
-    vmul.f32 s10, s11, s10
+    /* this xi = (xi-1 + S/xi-1)/2 */
+    vmov s13, s12
+    vdiv.f32 s12, s10, s12 
+    vadd.f32 s12, s12, s13
+    vmul.f32 s12, s12, s11
 
+    ldr r0, =xNot                /* save the value for next iteration */
+    vstr s12, [r0]
 
     /* for printing */
-    vcvt.f64.f32 d0, s10
+    vcvt.f64.f32 d0, s12
 
     vmov r2, r3, d0
     ldr r0, =floatRead
     bl printf
+
+    sub r5, r5, #1
+    cmp r5, #0
+    bne bLoop
+
       
 
     pop {r4, r5, r6,  lr}
