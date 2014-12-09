ac9f081678db7b9ac968e74000ceeecdc70815f5
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-08 17:45:06 -0800

    Signed off by Juan Ruiz

---
+++ CSC17A/Final/Final/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final
+//
+//  Created by Juan Ruiz on 12/8/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
