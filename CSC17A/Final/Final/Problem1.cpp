ea6fe3741e5ed05257dd1168d9e0e782ef6990a0
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-08 17:46:14 -0800

    Signed off by Juan Ruiz

---
+++ CSC17A/Final/Final/Problem1.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem1.cpp
+//  Final
+//
+//  Created by Juan Ruiz on 12/8/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#include "Problem1.h"
---
+++ CSC17A/Final/Final/Problem1.h
@@ -0,0 +1,14 @@
+//
+//  Problem1.h
+//  Final
+//
+//  Created by Juan Ruiz on 12/8/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#ifndef __Final__Problem1__
+#define __Final__Problem1__
+
+#include <stdio.h>
+
+#endif /* defined(__Final__Problem1__) */
