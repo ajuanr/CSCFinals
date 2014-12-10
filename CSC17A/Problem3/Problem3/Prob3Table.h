b3590c042a4ed32b8df0827f16c9b57bef6f78f8
Juan Ruiz <j.ruiz3750@gmail.com>
2014-12-10 12:19:13 -0800

    Signed off by Juan Ruiz

---
+++ CSC17A/Problem3/Problem3/Prob3Table.h
@@ -0,0 +1,33 @@
+//
+//  Prob3Table.h
+//  Problem3
+//
+//  Created by Juan on 12/10/14.
+//  Copyright (c) 2014 Juan. All rights reserved.
+//
+
+#ifndef Problem3_Prob3Table_h
+#define Problem3_Prob3Table_h
+
+
+template<class T>
+class Prob3Table
+{
+protected:
+    int rows;                                 //Number of rows in the table
+    int cols;                                 //Number of cols in the table
+    T *rowSum;                                //RowSum array
+    T *colSum;                                //ColSum array
+    T *table;                                 //Table array
+    T grandTotal;                             //Grand total
+    void calcTable(void);                     //Calculate all the sums
+public:
+    Prob3Table(char *,int,int);               //Constructor then Destructor
+    ~Prob3Table(){delete [] table;delete [] rowSum;delete [] colSum;};
+    const T *getTable(void){return table;};
+    const T *getRowSum(void){return rowSum;};
+    const T *getColSum(void){return colSum;};
+    T getGrandTotal(void){return grandTotal;};
+};
+
+#endif
---
+++ CSC17A/Problem3/Problem3/Prob3TableInherited.h
@@ -0,0 +1,25 @@
+//
+//  Prob3TableInherited.h
+//  Problem3
+//
+//  Created by Juan on 12/10/14.
+//  Copyright (c) 2014 Juan. All rights reserved.
+//
+
+#ifndef Problem3_Prob3TableInherited_h
+#define Problem3_Prob3TableInherited_h
+
+#include "Prob3Table.h"
+
+template<class T>
+class Prob3TableInherited:public Prob3Table<T>
+{
+protected:
+    T *augTable;                                  //Augmented Table with sums
+public:
+    Prob3TableInherited(char *,int,int);          //Constructor
+    ~Prob3TableInherited(){delete [] augTable;};  //Destructor
+    const T *getAugTable(void){return augTable;};
+};
+
+#endif
