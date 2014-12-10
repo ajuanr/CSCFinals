d3a398f9f5d4c34c2b722d012de50087ca8e1400
Juan Ruiz <j.ruiz3750@gmail.com>
2014-12-10 12:02:56 -0800

    Signed off by Juan ruiz

--- CSC17A/Problem2/Problem2/Prob2Sort.h
+++ CSC17A/Problem2/Problem2/Prob2Sort.h
@@ -12,6 +12,8 @@
 #include <iostream>
 
 using namespace std;
+template <class T>
+void swap(T*, T*, bool);
 
 template<class T>
 class Prob2Sort
@@ -25,13 +27,42 @@ public:
     T * sortArray(const T*,int,int,int,bool);   //Sorts a 2 dimensional array represented as a 1 dim array
 };
 
+
 template<class T>
 T * Prob2Sort<T>::sortArray(const T *array, int col, bool desc) {
-    int size = sizeof(array)/ sizeof(T);
-    cout << "Size is: " << size << endl;
+    int colSize = 15;
+    int rowSize = 10;
+    T *out = new T [colSize*rowSize];
     
-    T* out = new T[5];
+    // copy the array
+    for (int i = 0; i !=colSize*rowSize; ++i) {
+        out[i] = array[i];
+    }
+    
+    for ( int j = 0; j != 10; ++j) {
+    for (int i = 0; i != 9; ++i) {
+          cout << "Num: " << *(out+((i*colSize)+ col)) << " " << *(out+(((i+1)*colSize)+ col)) <<  endl;
+        swap((out+(i*colSize)+ col), (out+((i+1)*colSize)+ col), !desc );
+        cout << "swap: " << *(out+((i*colSize)+ col)) << " " << *(out+(((i+1)*colSize)+ col)) <<  endl;
+//            T temp = *(out+(i*colSize)+ col);
+//            *(out+(i*colSize)+ col) = *(out+(i*colSize)+ col+1);
+//            *(out+(i*colSize)+ col+1) = temp;
+        }
+    }
+    
     return out;
 }
+
+template<class T>
+void swap(T *a, T *b, bool desc) {
+    if ( a > b) {
+        cout << "True\n";
+        T temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+}
+
+
 
 #endif /* defined(__Problem2__Prob2Sort__) */
--- CSC17A/Problem2/Problem2/main.cpp
+++ CSC17A/Problem2/Problem2/main.cpp
@@ -13,21 +13,52 @@
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
-    char *test = new char [10];
+    Prob2Sort<char> rc;
     bool ascending = true;
-    
-    Prob2Sort<char> p2;
+
     
     ifstream infile;
     infile.open("Problem2.txt",ios::in);
-    if (infile.is_open()) cout << "Success\n";
+    if (infile.is_open()) {cout << "Success\n";}
     else cout << "Failed\n";
     
-    char n = 143;
-    cout << "N is: " << n<< endl;
+    //p2.sortArray(test, 5, ascending);
     
-    p2.sortArray(test, 5, ascending);
+    char *ch2=new char[10*16];
+    char *ch2p=ch2;
+    while(infile.get(*ch2)){
+        cout<<*ch2;ch2++;
+    }
+    cout << endl;
+    cout<<"Sorting on which column"<<endl;
+    int column;
+    cin>>column;
+    char *zc=rc.sortArray(ch2p,column,ascending);
+
+    for (int i = 0; i != 150; ++i) {
+        cout << zc[i];
+        if ( i %15 ==0) cout << endl;
+    }
+    cout << endl;
+    
+    int a = 5;
+    int b = 5;
+    swap(a,b, ascending);
+    cout << a << "b " << b << endl;
+    
+//    for(int i=0;i<10;i++)
+//    {
+//        for(int j=0;j<16;j++)
+//        {
+//            cout<<zc[i*16+j];
+//        }
+//    }
+    delete []zc;
+    cout<<endl;
+    
+    infile.close();
+    cout << endl;
+    
     
     return 0;
 }
