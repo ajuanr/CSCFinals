626e2c67700111fd5d149a31b9a49c69096393f4
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-10 20:38:54 -0800

    Started Problem 4

---
+++ CSC17A/Problem4/Problem4/SavingsAccount.cpp
@@ -0,0 +1,9 @@
+//
+//  SavingsAccount.cpp
+//  Problem4
+//
+//  Created by Juan Ruiz on 12/10/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#include "SavingsAccount.h"
---
+++ CSC17A/Problem4/Problem4/SavingsAccount.h
@@ -0,0 +1,14 @@
+//
+//  SavingsAccount.h
+//  Problem4
+//
+//  Created by Juan Ruiz on 12/10/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#ifndef __Problem4__SavingsAccount__
+#define __Problem4__SavingsAccount__
+
+#include <stdio.h>
+
+#endif /* defined(__Problem4__SavingsAccount__) */
