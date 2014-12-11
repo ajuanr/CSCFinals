6c3041294040cb08d4aa88acb53eed63c858f957
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-10 23:54:29 -0800

    Signed off by Juan Ruiz

--- CSC17A/Problem5/Problem5/main.cpp
+++ CSC17A/Problem5/Problem5/main.cpp
@@ -24,18 +24,18 @@ int main(int argc, const char * argv[]) {
     Mark.CalculatePay(Mark.setHourlyRate(60.0),
                       Mark.setHoursWorked(25));
     Mark.toString();
-//
-//    Employee Mary("Mary","VP",50.0);
-//    Mary.toString();
-//    Mary.CalculatePay(Mary.setHourlyRate(50.0),
-//                      Mary.setHoursWorked(40));
-//    Mary.toString();
-//    Mary.CalculatePay(Mary.setHourlyRate(50.0),
-//                      Mary.setHoursWorked(50));
-//    Mary.toString();
-//    Mary.CalculatePay(Mary.setHourlyRate(50.0),
-//                      Mary.setHoursWorked(60));
-//    Mary.toString();
+
+    Employee Mary("Mary","VP",50.0);
+    Mary.toString();
+    Mary.CalculatePay(Mary.setHourlyRate(50.0),
+                      Mary.setHoursWorked(40));
+    Mary.toString();
+    Mary.CalculatePay(Mary.setHourlyRate(50.0),
+                      Mary.setHoursWorked(50));
+    Mary.toString();
+    Mary.CalculatePay(Mary.setHourlyRate(50.0),
+                      Mary.setHoursWorked(60));
+    Mary.toString();
     
     return 0;
 }
