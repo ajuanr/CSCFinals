fa401ac9bbf92f9205c809e88a89d3e621026942
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-10 21:19:41 -0800

    Started Problem 5

---
+++ CSC17A/Problem5/Problem5/Employee.h
@@ -0,0 +1,14 @@
+//
+//  Employee.h
+//  Problem5
+//
+//  Created by Juan Ruiz on 12/10/14.
+//  Copyright (c) 2014 Juan Ruiz. All rights reserved.
+//
+
+#ifndef __Problem5__Employee__
+#define __Problem5__Employee__
+
+#include <stdio.h>
+
+#endif /* defined(__Problem5__Employee__) */
