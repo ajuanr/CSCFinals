edfc57080049a895edf7abd6a3981e8ceea86265
Juan Ruiz <j_ruiz05@yahoo.com>
2014-12-10 23:53:30 -0800

    Signed off by Juan Ruiz

--- CSC17A/Problem5/Problem5/Employee.cpp
+++ CSC17A/Problem5/Problem5/Employee.cpp
@@ -7,8 +7,11 @@
 //
 
 #include <iostream>
+#include <iomanip>
 #include "Employee.h"
+
 using namespace std;
+
 Employee::Employee(char name[], char job[], float rate)
 :HoursWorked(0), GrossPay(0), NetPay(0){
     int nullChar = 0;
@@ -24,11 +27,11 @@ Employee::Employee(char name[], char job[], float rate)
     }
     JobTitle[nullChar] = '\0';
 
-    HourlyRate = rate;
+    HourlyRate = setHourlyRate(rate);
 }
 
 int  Employee::setHoursWorked(int hours) {
-    if (hours >=0 ) {
+    if (hours >0 && hours < 84 ) {
         HoursWorked = hours;
         return HoursWorked;
     }
@@ -36,10 +39,65 @@ int  Employee::setHoursWorked(int hours) {
     return HoursWorked;
 }
 float Employee::setHourlyRate(float rate) {
-    if( rate > 0) {
+    if (rate > 0 && rate < 200) {
         HourlyRate = rate;
         return HourlyRate;
     }
     cout << "Unacceptable hourly rate\n";
     return HourlyRate;
+}
+
+void Employee::toString() {
+    cout << "Name = " << MyName << " Job Title= " << JobTitle << endl
+         << "Hourly rate = " << fixed << setprecision(0) << HourlyRate
+         << " hours worked = " << HoursWorked
+    << " Gross Pay= " << GrossPay << " Net Pay= " << NetPay << endl;
+}
+
+float Employee::getGrossPay(float rate, int hours) {
+    //cout << "RATE*HOURS" << rate * hours << endl;
+    float totalPay = 0;
+    if (hours > 50) {
+        float newRate = rate * 2;
+        int hoursOvr = hours-50;
+        totalPay += (newRate * hoursOvr);
+        hours -= hoursOvr;
+    }
+    if (hours > 40) {
+        float newRate = rate * 1.5;
+        int hoursOvr = hours - 40;
+        totalPay += (newRate * hoursOvr);
+        hours -= hoursOvr;
+        
+        }
+    totalPay+= (rate * hours);
+    GrossPay = totalPay;
+    
+    return GrossPay;
+}
+
+double Employee::Tax(float pay) {
+    float tax = 0;
+    if ( pay > 1000) {
+        float taxable = pay - 1000;
+        pay -= taxable;
+        tax+= taxable * .3;
+    }
+    if ( pay > 500) {
+        float taxable = pay - 500;
+        tax+= taxable * .2;
+        pay -= taxable;
+    }
+    tax += pay *.1;
+    return tax;
+    
+}
+
+float Employee::getNetPay(float pay) {
+    NetPay = getGrossPay(HourlyRate, HoursWorked) - Tax(pay);
+    return NetPay;
+}
+
+float Employee::CalculatePay(float rate, int hours) {
+    return getNetPay(getGrossPay(setHourlyRate(rate), setHoursWorked(hours)));
 }
